b7b4fce7aeb3ef6e6f38f157365bf7686654ea58
tobias <>
2019-12-03 15:14:27 +0100

    API file

---
+++ api.py
@@ -0,0 +1,12 @@
+import requests
+
+response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo")
+
+# Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
+# See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+if response.status_code != 200:
+    raise ValueError("Could not retrieve data, code:", response.status_code)
+
+# The service sends JSON data, we parse that into a Python datastructure
+raw_data = response.json()
+print (raw_data)
