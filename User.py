b573197268abb91f5a09cda9fe7cbdea9eb97efb
jbijman <jbijman@deloitte.nl>
2019-12-03 16:50:39 +0100

    Initiate self.portfolio with class portfolio

--- User.py
+++ User.py
@@ -1,23 +1,21 @@
 from Order import Order
+from Portfolio import Portfolio
 
 class User:
     def __init__(self, name, age, balance):
         self.name = name
         self.age = age
         self.balance = balance
-        self.portfolio = []
-
-    def make_portfolio(self):
-        """Make a portfolio"""
+        self.portfolio = Portfolio()
 
     def transaction(self, order, quantity, investment):
         """"Make transaction"""
         o = Order(order, quantity, investment)
-        transaction_amount = o.market_order()
-        self.balance = self.balance - transaction_amount
+        transaction_amount, price, date = o.market_order()
 
+        self.balance = self.balance - transaction_amount
         #if transaction_status == True:
-        #    update_portfolio()
+        self.portfolio.add_transaction(order, quantity, investment, price, date)
         #else:
         #    return error
 
@@ -31,7 +29,13 @@ class User:
 p1 = User("John", 36, 10000)
 
 print(p1.balance)
-p1.transaction(1, 10, 'x')
+p1.transaction('Sell', 10, 'x')
+p1.transaction('Buy', 20, 'y')
 print(p1.name)
-print(p1.age)
-print(p1.balance)
+
+print(p1.portfolio.portfolio)
+
+
+stock_indx = ['x', 'y', 'i', 'sdfaa']
+
+print('www.xyz.com/mystock=%s' % (stock_indx[3]) )
