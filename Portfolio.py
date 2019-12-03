571f7e3c8d82e168df8c4759d39e32ccc1fd6d57
jbijman <jbijman@deloitte.nl>
2019-12-03 16:50:17 +0100

    Added price and date to transaction portfolio

--- Portfolio.py
+++ Portfolio.py
@@ -1,7 +1,9 @@
 class Portfolio:
     def __init__(self):
-        portfolio = []
+        self.portfolio = []
 
-    def add_transaction(self, order, quantity, investment):
-        self.portfolio.append([order, quantity, investment])
+    def add_transaction(self, order, quantity, investment, price, date):
+        self.portfolio.append([order, quantity, investment, price, date])
+
+
 
