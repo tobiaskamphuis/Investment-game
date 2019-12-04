fba726407ab2db85dba166896bfeb28e4e48b5f5
ehelling <esther.helling@gmail.com>
2019-12-04 14:23:06 +0100

    commit changes to order and user

--- investmentGame/Order.py
+++ investmentGame/Order.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+
 class Order:
     def __init__(self, order, order_type, quantity, investment):
         self.order = order
@@ -7,40 +8,38 @@ class Order:
         self.quantity = quantity
         self.investment = investment
 
-    def market_order(self):
-        """buy and sell at market price"""
-        price = 150 #get_price(investment)
-        dt = date.today().strftime("%m/%d/%Y")
-        return price * self.quantity, price, dt
+    def execution_order(self, execution_price = 0, execution_date = 0):
+        if self.order_type == 'market_order':
+            """buy and sell at market price"""
+            price = 150  # get_price(investment)
+            dt = date.today().strftime("%d/%m/%Y")
+            return price * self.quantity, price, dt
 
-    def limit_order(self, execution_price, order_date):
-        """buy till max price or sell from min price during timeframe"""
-        """ limit order checks if price on order date """
-        """ require to retrieve minimum and maximum price in last week of stock"""
-        dt = date.today().strftime("%d/%m/%Y")
-        while order_date >= dt:
-            if self.order == 'Buy':
-                price = 150  # retrieve minimum price in last week
-                if execution_price >= price:
-                    return price * self.quantity, price, dt
-                elif execution_price < price:
-                    return 'error'
-            if self.order == 'Sell':
-                price = 150  # retrieve maximum price in last week
-                if execution_price <= price:
-                    return price * self.quantity, price, dt
-                elif execution_price > price:
-                    return 'error'
+        if self.order_type == 'limit_order':
+            dt = date.today().strftime("%d/%m/%Y")
+            while execution_date >= dt:
+                if self.order == 'Buy':
+                    price = 150  # retrieve minimum price in last week
+                    if execution_price >= price:
+                        return price * self.quantity, price, dt
+                    elif execution_price < price:
+                        return 'error'
+                if self.order == 'Sell':
+                    price = 150  # retrieve maximum price in last week
+                    if execution_price <= price:
+                        return price * self.quantity, price, dt
+                    elif execution_price > price:
+                        return 'error'
 
 
 order1 = Order('Buy', 'market_order', 10, 'SMA')
 
 print(order1.quantity)
 print(order1.investment)
-print(order1.market_order())
+print(order1.execution_order())
 
 order2 = Order('Buy', 'limit_order', 10, 'SMA')
 
 print(order2.quantity)
 print(order2.investment)
-print(order2.limit_order(140, '04/12/2019'))
+print(order2.execution_order(160, '04/12/2019'))
--- investmentGame/User.py
+++ investmentGame/User.py
@@ -1,6 +1,7 @@
 from investmentGame.Order import Order
 from investmentGame.Portfolio import Portfolio
 
+
 class User:
     def __init__(self, name, age, balance):
         self.name = name
@@ -8,10 +9,10 @@ class User:
         self.balance = balance
         self.portfolio = Portfolio()
 
-    def transaction(self, order, quantity, investment):
+    def transaction(self, order, order_type, quantity, investment, execution_price = 0, execution_date = 0):
         """"Make transaction"""
-        o = Order(order, quantity, investment)
-        transaction_amount, price, date = o.market_order()
+        o = Order(order, order_type, quantity, investment)
+        transaction_amount, price, date = o.execution_order(execution_price, execution_date)
         self.balance = self.balance - transaction_amount
         self.portfolio.add_transaction(order, quantity, investment, price, date)
 
@@ -23,11 +24,9 @@ class User:
 
 
 p1 = User("John", 36, 10000)
-p1.transaction('Sell', 10, 'x')
-p1.transaction('Buy', 20, 'y')
+p1.transaction('Sell', 'market_order', 10, 'x')
+p1.transaction('Buy', 'limit_order',  20, 'y', 150, '12/04/2019')
 p1.portfolio.store_transactions()
-
-
 
 print(p1.balance)
 print(p1.name)
