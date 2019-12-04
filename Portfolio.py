0679a9d298649dff46a2e93ddfd095f0e2ababd4
jbijman <jbijman@deloitte.nl>
2019-12-04 10:40:41 +0100

    function to store transactions into csv file

--- Portfolio.py
+++ Portfolio.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class Portfolio:
     def __init__(self):
         self.portfolio = []
@@ -5,5 +8,5 @@ class Portfolio:
     def add_transaction(self, order, quantity, investment, price, date):
         self.portfolio.append([order, quantity, investment, price, date])
 
-
-
+    def store_transactions(self):
+        pd.DataFrame(self.portfolio).to_csv("transactions.csv")
