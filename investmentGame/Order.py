48a1801868a09a7e46eb69b2ba136341b4e9e539
tobias <>
2019-12-05 13:49:35 +0100

    Update order with API

--- investmentGame/Order.py
+++ investmentGame/Order.py
@@ -1,6 +1,5 @@
 from datetime import date
-from  api import responses
-
+from investmentGame.api import pricemax, pricemin, pricelatest, getprices
 
 class Order:
     def __init__(self, order, order_type, quantity, investment):
@@ -9,38 +8,41 @@ class Order:
         self.quantity = quantity
         self.investment = investment
 
-    def execution_order(self, execution_price = 0, execution_date = 0):
+    def execution_order(self, execution_price = 0):
         if self.order_type == 'market_order':
             """buy and sell at market price"""
-            price = 150  # get_price(investment)
+            getprices(self.investment)
+            price = pricelatest(self.investment) # get_price(investment)
             dt = date.today().strftime("%d/%m/%Y")
             return price * self.quantity, price, dt
 
         if self.order_type == 'limit_order':
             dt = date.today().strftime("%d/%m/%Y")
-            while execution_date >= dt:
-                if self.order == 'Buy':
-                    price = 150  # retrieve minimum price in last week
-                    if execution_price >= price:
-                        return price * self.quantity, price, dt
-                    elif execution_price < price:
-                        return 'error'
-                if self.order == 'Sell':
-                    price = 150  # retrieve maximum price in last week
-                    if execution_price <= price:
-                        return price * self.quantity, price, dt
-                    elif execution_price > price:
-                        return 'error'
+            if self.order == 'Buy':
+                getprices(self.investment)
+                price = pricemin(self.investment)  # retrieve minimum price in last week
+                if execution_price >= price:
+                    return price * self.quantity, price, dt
+                elif execution_price < price:
+                    return 'error'
+            if self.order == 'Sell':
+                getprices(self.investment)
+                price = pricemax(self.investment)  # retrieve maximum price in last week
+                if execution_price <= price:
+                    return price * self.quantity, price, dt
+                elif execution_price > price:
+                    return 'error'
 
-
-order1 = Order('Buy', 'market_order', 10, 'SMA')
+#
+order1 = Order('Buy', 'market_order', 10, 'AAPL')
 
 print(order1.quantity)
 print(order1.investment)
 print(order1.execution_order())
 
-order2 = Order('Buy', 'limit_order', 10, 'SMA')
+order2 = Order('Buy', 'limit_order', 10, 'AAPL')
 
 print(order2.quantity)
 print(order2.investment)
-print(order2.execution_order(160, '04/12/2019'))
+print(order2.execution_order(261))
+
